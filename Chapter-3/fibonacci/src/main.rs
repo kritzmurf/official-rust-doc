3e4257ae4d1a909b1f266a643b464bca07279c13
Patrick Murphy <pmurphy@intelligentag.com>
2024-06-06 14:16:13 -0500

    initial commit for fibonacci practice program

--- Chapter-3/fibonacci/src/main.rs
+++ Chapter-3/fibonacci/src/main.rs
@@ -1,3 +1,40 @@
+//NOTE: this will overflow with a high enough input. Should add checker
+
 fn main() {
-    println!("Hello, world!");
+    println!("{0}", fib_calc(accept_input()));
+
+}
+
+
+fn accept_input() -> i32 {
+    let mut input = String::new();
+    loop {
+        println!("Please enter which fibonacci number to print");
+
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let parsed_input: i32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input. Please input an integer");
+                input.clear();
+                continue;
+            },
+        };
+        break parsed_input
+    }
+}
+
+fn fib_calc(nth : i32) -> i32 {
+    let mut prev: i32 = 0;
+    let mut sum: i32 = 1;
+
+    for _ in 0..nth {
+        let temp: i32 = sum;
+        sum += prev;
+        prev = temp;
+    }
+    sum
 }
